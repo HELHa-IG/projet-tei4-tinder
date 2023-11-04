24255fac1f604f4117f42431b97a3f1be5f4d3e7
Zakaria Smaali <101876655+Zakariasma@users.noreply.github.com>
2023-11-04 09:20:38 +0100

    Login and register ON

---
+++ Tinder/Tinder/Controllers/AuthController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Tinder.Models;
+
+namespace Tinder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static Users user = new Users();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpPost("register")]
+        public ActionResult<Users> Register(Users request)
+        {
+            string passwordHash
+                = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            user.Email = request.Email;
+            user.Password = passwordHash;
+            user.Hobby = request.Hobby;
+            user.Role = "User";
+            user.Prenom = request.Prenom;
+            user.Nom = request.Nom;
+            user.PhotoJson = request.PhotoJson;
+            user.Locality = request.Locality;
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<Users> Login(Users request)
+        {
+            if (user.Email != request.Email)
+            {
+                return BadRequest("User not found.");
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.Password))
+            {
+                return BadRequest("Wrong password.");
+            }
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(Users user)
+        {
+            List<Claim> claims = new List<Claim> {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Role, "User"),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+
+
+}
+
